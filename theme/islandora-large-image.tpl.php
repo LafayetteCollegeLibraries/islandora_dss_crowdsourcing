120e240ddf9176e84712f30c9d0d5f6d1dbb42fd
jrgriffiniii <griffinj@lafayette.edu>
2014-09-10 09:55:33 -0400

    SYR-2 #time 3m Tracking the template for Large Image Objects

---
+++ theme/islandora-large-image.tpl.php
@@ -0,0 +1,89 @@
+<?php
+
+/**
+ * @file
+ * This is the template file for the object page for large image
+ *
+ * Available variables:
+ * - $islandora_object: The Islandora object rendered in this template file
+ * - $islandora_dublin_core: The DC datastream object
+ * - $dc_array: The DC datastream object values as a sanitized array. This
+ *   includes label, value and class name.
+ * - $islandora_object_label: The sanitized object label.
+ * - $parent_collections: An array containing parent collection(s) info.
+ *   Includes collection object, label, url and rendered link.
+ * - $islandora_thumbnail_img: A rendered thumbnail image.
+ * - $islandora_content: A rendered image. By default this is the JPG datastream
+ *   which is a medium sized image. Alternatively this could be a rendered
+ *   viewer which displays the JP2 datastream image.
+ *
+ * @see template_preprocess_islandora_large_image()
+ * @see theme_islandora_large_image()
+ */
+
+?>
+
+<div class="islandora-large-image-object islandora">
+
+  <div class="islandora-large-image-content-wrapper clearfix">
+    <?php if ($islandora_content): ?>
+      <div class="islandora-large-image-content">
+        <?php print $islandora_content; ?>
+      </div>
+    <?php endif; ?>
+  </div>
+
+  <fieldset class="collapsible collapsed islandora-large-image-metadata">
+    <h3 class="islandora-image-details">Item Details</h3>
+    <div class="fieldset-wrapper">
+      <dl class="islandora-inline-metadata islandora-large-image-fields">
+        <?php $row_field = 0; ?>
+
+        <?php if (isset($mods_object)): ?>
+
+          <?php foreach ($mods_object as $key => $value): ?>
+
+            <dt class="<?php
+		       print $value['class'];
+		       print $row_field == 0 ? ' first' : '';
+		       if($value['label'] != ''):
+
+					     print ' islandora-inline-metadata-displayed';
+		       endif;
+		       ?>">
+
+              <?php print $value['label']; ?>
+            </dt>
+            <dd class="<?php print $value['class']; ?><?php print $row_field == 0 ? ' first' : ''; ?>">
+
+              <?php if(array_key_exists('date_value', $value)): ?>
+
+	        <?php print $value['facet_href']; ?>
+	      <?php else: ?>
+
+                <?php
+
+		if(array_key_exists('facet', $value)) {
+
+		  print $value['facet_href'];
+		} elseif(array_key_exists('href', $value)) {
+
+                  print l($value['value'], $value['href']);
+		} else {
+
+		  print $value['value'];
+		}
+		?>
+	      <?php endif; ?>
+            </dd>
+            <?php $row_field++; ?>
+          <?php endforeach; ?>
+        <?php endif; ?>
+      </dl>
+    </div>
+  </fieldset>
+  
+  <div class="crowdsourcing">
+    <?php print $crowdsourcing; ?>
+  </div>
+</div>
